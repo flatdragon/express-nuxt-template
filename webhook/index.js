15b3d40a399158775f966874333c779210f847b8
Kochetygov Andrew <flatdragon@yandex.ru>
2021-04-08 18:13:40 +0300

    update webhook debugging messages

--- webhook/index.js
+++ webhook/index.js
@@ -14,11 +14,15 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
+  console.log(`\n---------- Incoming Webhook Call at ${Date.now()} ----------\n`)
+  console.log('Verifiying signature...')
+
   const githubSignature = req.headers['x-hub-signature']
   const calculatedSignature = 'sha1=' + crypto.createHmac('sha1', process.env.WEBHOOK_SECRET).update(JSON.stringify(req.body), 'utf-8').digest('hex')
 
   console.log('-- X-Hub-Signature: ' + githubSignature)
   console.log('-- Calculated Signature: ' + calculatedSignature)
+  console.log('\n')
 
   if (githubSignature !== calculatedSignature) {
     console.log('Invalid signature. Aborting...\n')
