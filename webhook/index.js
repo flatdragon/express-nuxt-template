c66e4f93a59b5d14742dc7cbd85589221783bd59
Kochetygov Andrew <flatdragon@yandex.ru>
2021-04-08 17:49:50 +0300

    update webhook logic

---
+++ webhook/index.js
@@ -0,0 +1,36 @@
+const ip = require('ip')
+const http = require('http')
+const crypto = require('crypto')
+const express = require('express')
+const { exec } = require('child_process')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).send('Webhook is running...\n')
+})
+
+app.post('/', (req, res) => {
+  console.log('check')
+
+  const githubSignature = req.headers['x-hub-signature']
+  const calculatedSignature = 'sha1=' + crypto.createHmac('sha1', process.env.WEBHOOK_SECRET).update(JSON.stringify(req.body), 'utf-8').digest('hex')
+
+  console.log('-- X-Hub-Signature: ' + githubSignature)
+  console.log('-- Calculated Signature: ' + calculatedSignature)
+
+  if (githubSignature !== calculatedSignature) {
+    console.log('Invalid signature. Aborting...\n')
+    res.status(403).send()
+  } else {
+    console.log('Valid signature. Init project syncing...\n')
+    res.status(200).send()
+  }
+})
+
+const webhook = http.createServer(app)
+
+const HOST = ip.address()
+const PORT = process.env.PORT ?? 4000
+
+webhook.listen(PORT, HOST, () => console.log(`Server running at http://${HOST}:${PORT}`))
